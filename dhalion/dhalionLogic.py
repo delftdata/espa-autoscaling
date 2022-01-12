9f3eb9dac6c29b357482682f0a26b3792adeeade
wybekoper <wybe.koper@gmail.com>
2022-01-12 17:21:11 +0100

    Added patch to cluser role

--- dhalion/dhalionLogic.py
+++ dhalion/dhalionLogic.py
@@ -8,7 +8,8 @@ from kubernetes import client, config
 while True:
         print('Looping')
         print(os.environ['COOLDOWN'])
-        backpressure_query = requests.get("http://prometheus-server/api/v1/query?query=max(avg_over_time(flink_taskmanager_job_task_backPressuredTimeMsPerS>        backpressure_value = backpressure_query.json()["data"]["result"][0]["value"][1]
+        backpressure_query = requests.get("http://prometheus-server/api/v1/query?query=max(avg_over_time(flink_taskmanager_job_task_backPressuredTimeMsPerSecond[5m]))")
+        backpressure_value = backpressure_query.json()["data"]["result"][0]["value"][1]
         print("backpressure value")
         print(backpressure_value)
 
@@ -19,6 +20,12 @@ while True:
         for i in ret.items:
                 print(i.spec.replicas, i.metadata.namespace, i.metadata.name)
 
+        try:
+                body = {"spec":{"replicas":3}}
+                api_response = v1.patch_namespaced_deployment_scale(name="flink-taskmanager", namespace="default", body=body, pretty=True)
+                print(api_response)
+        except Exception as e:
+                print(e)
 
         time.sleep(5)
         raise Exception('Exception in loop')
