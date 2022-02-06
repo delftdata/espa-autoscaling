5ff30e6a9205b1ecf23e7c32b1c550c783348938
wybekoper <wybe.koper@gmail.com>
2022-02-06 19:06:06 +0100

    Changed kafka consumer to kafka connector

--- reactive-mode-demo-jobs/src/main/java/org/apache/flink/DemoJob.java
+++ reactive-mode-demo-jobs/src/main/java/org/apache/flink/DemoJob.java
@@ -18,14 +18,16 @@
 
 package org.apache.flink;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.DiscardingSink;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.util.Collector;
 
 import org.slf4j.Logger;
@@ -43,14 +45,27 @@ public class DemoJob {
         env.disableOperatorChaining();
         env.enableCheckpointing(30_000L);
 
+        //        DataStream<String> stream =
+        //                env.addSource(
+        //                                new FlinkKafkaConsumer<>(
+        //                                        params.get("topic"),
+        //                                        new SimpleStringSchema(),
+        //                                        params.getProperties()))
+        //                        .setMaxParallelism(1)
+        //                        .name("Kafka");
+
+        KafkaSource<String> source =
+                KafkaSource.<String>builder()
+                        .setBootstrapServers("kafka-service:9092")
+                        .setTopics("topic")
+                        .setGroupId("yolo")
+                        .setStartingOffsets(OffsetsInitializer.earliest())
+                        .setValueOnlyDeserializer(new SimpleStringSchema())
+                        .build();
+
         DataStream<String> stream =
-                env.addSource(
-                                new FlinkKafkaConsumer<>(
-                                        params.get("topic"),
-                                        new SimpleStringSchema(),
-                                        params.getProperties()))
-                        .setMaxParallelism(1)
-                        .name("Kafka");
+                env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
+                        .setParallelism(1);
 
         // stream.flatMap(new ThroughputLogger(16, params.getLong("logfreq", 10_000)))
         //         .setMaxParallelism(1)
