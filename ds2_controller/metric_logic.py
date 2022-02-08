bac73326c6807348f08dbad4bd8a3acf9c25666b
wybekoper <wybe.koper@gmail.com>
2022-02-08 22:10:46 +0100

    created ds2 script

--- ds2_controller/metric_logic.py
+++ ds2_controller/metric_logic.py
@@ -3,6 +3,7 @@ import json
 import math
 import time
 import csv
+import subprocess
 
 
 # extract metrics from prometheus query
@@ -57,7 +58,7 @@ for key in output_rates_per_operator:
 # print(true_output_rate)
 
 
-with open('./examples/offline/flink_test_rates_2_tm.log', 'w', newline='') as f:
+with open('./examples/demo/flink_rates.log', 'w', newline='') as f:
     writer = csv.writer(f)
     header = ["# operator_id", "operator_instance_id", "total_number_of_operator_instances", "epoch_timestamp", "true_processing_rate", "true_output_rate", "observed_processing_rate", "observed_output_rate"]
     writer.writerow(header)
@@ -70,4 +71,57 @@ with open('./examples/offline/flink_test_rates_2_tm.log', 'w', newline='') as f:
         writer.writerow(row)
 
 
+operator_set = set()
+topology_order = []
+topology_parallelism = {}
+with open('./examples/demo/flink_topology.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ')
+    for index, row in enumerate(reader):
+        if index == 0:
+            continue
+        row_values = row[0].split(",")
+        topology_parallelism[row_values[0]] = row_values[2]
+        topology_parallelism[row_values[3]] = row_values[5]
+        if row_values[0] not in operator_set:
+            topology_order.append(row_values[0])
+            operator_set.add(row_values[0])
+        if row_values[3] not in operator_set:
+            topology_order.append(row_values[3])
+            operator_set.add(row_values[3])
 
+
+with open('./examples/demo/flink_topology2.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    header = ["# operator_id1","operator_name1","total_number_of_operator_instances1","operator_id2","operator_name2","total_number_of_operator_instances2"]
+    writer.writerow(header)
+    for i in range(0, len(topology_order) - 1):
+        row = [topology_order[i], topology_order[i], int(processors_per_operator[topology_order[i]]), topology_order[i + 1], topology_order[i + 1], int(processors_per_operator[topology_order[i + 1]])]
+        writer.writerow(row)
+
+
+ds2_model_result = subprocess.run(["cargo", "run", "--release", "--bin", "policy", "--", "--topo", "examples/demo/flink_topology.csv", "--rates", "examples/demo/flink_rates.log", "--system", "flink"], capture_output=True)
+output_text = ds2_model_result.stdout.decode("utf-8").replace("\n", "")
+output_text_values = output_text.split(",")
+suggested_parallelism = {}
+
+for i in range(0, len(output_text_values), 2):
+    suggested_parallelism[output_text_values[i]] = output_text_values[i + 1]
+
+print(suggested_parallelism)
+job_id_json = requests.get("http://localhost:8081/jobs/")
+job_id = job_id_json.json()['jobs'][0]['id']
+
+savepoint = requests.post("http://localhost:8081/jobs/" + job_id + "/savepoints")
+
+#sleep so savepoint can be taken
+time.sleep(5)
+trigger_id = savepoint.json()['request-id']
+savepoint_name = requests.get("http://localhost:8081/jobs/" + job_id + "/savepoints/" + trigger_id)
+savepoint_name = savepoint_name.json()["operation"]["location"]
+
+print(job_id)
+print(trigger_id)
+print(savepoint_name)
+
+stop_request = requests.post("http://localhost:8081/jobs/" + job_id + "/stop")
+print(stop_request.text)
