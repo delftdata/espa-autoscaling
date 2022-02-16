116c19b303f0324411f4d021fb3b4a744d4f803a
wybekoper <wybe.koper@gmail.com>
2022-02-16 14:59:55 +0100

    Updated deployment script

--- google_cloud/HPA_experiments/deploy_HPA_experiment.sh
+++ google_cloud/HPA_experiments/deploy_HPA_experiment.sh
@@ -20,11 +20,12 @@ helm install grafana grafana --repo https://grafana.github.io/helm-charts --valu
 
 kubectl run workbench --image=ubuntu:21.04 -- sleep infinity
 
+kubectl expose deployment prometheus-server --type=LoadBalancer --name=my-external-prometheus
+kubectl expose deployment grafana --type=LoadBalancer --name=my-external-grafana
+
 echo "Waiting for everything to be ready"
   kubectl wait --timeout=5m --for=condition=ready pods --all
 
 kubectl exec workbench -- bash -c "apt update && apt install -y maven git htop iputils-ping wget net-tools && git clone https://github.com/WybeKoper/PASAF.git && cd PASAF/experiments && mvn clean install"
 
-# kubectl expose deployment hello-world --type=LoadBalancer --name=my-external-prometheus
-# kubectl expose deployment hello-world --type=LoadBalancer --name=my-external-grafana
-# kubectl expose deployment hello-world --type=LoadBalancer --name=my-external-flink
+# kubectl expose job flink-jobmanager  
