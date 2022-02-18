e6f62386b8831a1b9b72eece61591bbe0f7e1a8e
wybekoper <wybe.koper@gmail.com>
2022-02-18 11:56:46 +0100

    Containerized load generator

--- google_cloud/HPA_experiments/deploy_HPA_experiment.sh
+++ google_cloud/HPA_experiments/deploy_HPA_experiment.sh
@@ -18,14 +18,14 @@ helm install prometheus prometheus --repo https://prometheus-community.github.io
 
 helm install grafana grafana --repo https://grafana.github.io/helm-charts --values values-grafana.yaml --set-file dashboards.default.flink-dashboard.json=grafana-dashboard.json --set-file dashboards.default.scaling-dashboard.json=grafana-dashboard-auto.json
 
-kubectl run workbench --image=ubuntu:21.04 -- sleep infinity
-
 kubectl expose deployment prometheus-server --type=LoadBalancer --name=my-external-prometheus
 kubectl expose deployment grafana --type=LoadBalancer --name=my-external-grafana
 
 echo "Waiting for everything to be ready"
-  kubectl wait --timeout=5m --for=condition=ready pods --all
+  kubectl wait --timeout=52 --for=condition=ready pods --all
 
-kubectl exec workbench -- bash -c "apt update && apt install -y maven git htop iputils-ping wget net-tools && git clone https://github.com/WybeKoper/PASAF.git && cd PASAF/experiments && mvn clean install"
+# kubectl exec workbench -- bash -c "apt update && apt install -y maven git htop iputils-ping wget net-tools && git clone https://github.com/WybeKoper/PASAF.git && cd PASAF/experiments && mvn clean install"
 
 # kubectl expose job flink-jobmanager  
+
+kubectl apply -f workbench-deployment.yaml
--- google_cloud/commands.md
+++ google_cloud/commands.md
@@ -1,2 +1,5 @@
 kubectl expose deployment hello-world --type=LoadBalancer --name=my-service
 exposing service to internet
+
+constant mode:
+mvn exec:java -Dexec.mainClass="ch.ethz.systems.strymon.ds2.flink.nexmark.sources.BidSourceFunctionGeneratorKafka" -Dexec.args="--mode 0 --rate 100000"
