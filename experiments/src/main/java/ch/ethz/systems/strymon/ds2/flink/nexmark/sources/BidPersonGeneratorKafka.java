b16b8e36ab437b3665970e12396f53fe9b50eb4e
wybekoper <wybe.koper@gmail.com>
2022-03-04 20:00:25 +0100

    fixed person generator bug

--- experiments/src/main/java/ch/ethz/systems/strymon/ds2/flink/nexmark/sources/BidPersonGeneratorKafka.java
+++ experiments/src/main/java/ch/ethz/systems/strymon/ds2/flink/nexmark/sources/BidPersonGeneratorKafka.java
@@ -117,7 +117,7 @@ public class BidPersonGeneratorKafka {
                 }
             else{
                 for (int i = 0; i < current_rate; i++) {
-                    if (eventsCountSoFarPerson % 2 == 0) {
+                    if (eventsCountSoFarAuctions % 2 == 0) {
                         long eventTimestamp =
                                 config.timestampAndInterEventDelayUsForEvent(
                                         config.nextEventNumber(eventsCountSoFarPerson)).getKey();
