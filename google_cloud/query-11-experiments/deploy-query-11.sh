7f7ffd4a10b691f816edff3c5fee2fe68ae36657
wybekoper <wybe.koper@gmail.com>
2022-03-05 10:05:15 +0100

    Updated query deployment scripts

--- google_cloud/query-11-experiments/deploy-query-11.sh
+++ google_cloud/query-11-experiments/deploy-query-11.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-kubectl apply -f ./common-files/flink-configuration-configmap.yaml
-kubectl apply -f ./common-files/experiments-jobmanager.yaml
-kubectl apply -f ./common-files/jobmanager-rest-service.yaml
-kubectl apply -f ./common-files/jobmanager-service.yaml
-kubectl apply -f ./common-files/experiments-taskmanager.yaml
+cd ..
+cd common-files
+kubectl apply -f flink-configuration-configmap.yaml
+kubectl apply -f jobmanager-rest-service.yaml
+kubectl apply -f jobmanager-service.yaml
+kubectl apply -f experiments-taskmanager.yaml
 
 
-kubectl apply -f ./common-files/zookeeper-service.yaml
-kubectl apply -f ./common-files/zookeeper-deployment.yaml
-kubectl apply -f ./common-files/kafka-multi-broker.yaml
+kubectl apply -f zookeeper-service.yaml
+kubectl apply -f zookeeper-deployment.yaml
+kubectl apply -f kafka-multi-broker.yaml
 
-helm install prometheus prometheus --repo https://prometheus-community.github.io/helm-charts --values ./common-files/values-prometheus.yaml
+helm install prometheus prometheus --repo https://prometheus-community.github.io/helm-charts --values values-prometheus.yaml
 
-helm install grafana grafana --repo https://grafana.github.io/helm-charts --values ./common-files/values-grafana.yaml --set-file dashboards.default.flink-dashboard.json=./common-files/grafana-dashboard.json --set-file dashboards.default.scaling-dashboard.json=./common-files/grafana-dashboard-auto.json
+helm install grafana grafana --repo https://grafana.github.io/helm-charts --values values-grafana.yaml --set-file dashboards.default.flink-dashboard.json=grafana-dashboard.json --set-file dashboards.default.scaling-dashboard.json=grafana-dashboard-auto.json
 
 kubectl expose deployment prometheus-server --type=LoadBalancer --name=my-external-prometheus
 kubectl expose deployment grafana --type=LoadBalancer --name=my-external-grafana
@@ -24,6 +25,9 @@ kubectl wait --timeout=1m --for=condition=ready statefulset --all
 
 kubectl exec kafka-2 -- /opt/kafka/bin/kafka-topics.sh --create -zookeeper zoo1:2181  --replication-factor 1 --partitions 24 --topic bids_topic
 
-# kubectl expose job flink-jobmanager  
-
+# kubectl expose job flink-jobmanager
+cd ..
+cd query-1-experiments
+kubectl apply -f experiments-jobmanager.yaml
 kubectl apply -f workbench-deployment.yaml
+
