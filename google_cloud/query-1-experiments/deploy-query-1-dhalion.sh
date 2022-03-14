1018f916a32548fe587ddc399b884526525635cc
wybekoper <wybe.koper@gmail.com>
2022-03-14 09:32:38 +0100

    added dhalion deployment script

--- google_cloud/query-1-experiments/deploy-query-1-dhalion.sh
+++ google_cloud/query-1-experiments/deploy-query-1-dhalion.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+
+
+bash deploy-query-1.sh
+
 cd ..
 
 cd common-files
 
 kubectl apply -f dhalion_rbac_rules.yaml
-kubectl apply -f dhalion-service.yaml
-kubectl apply -f dhalion-deployment.yaml
+kubectl apply -f dhalion-deployment_v2.yaml
 
-cd ..
-cd query-1-experiments
 
-bash deploy-query-1.sh
