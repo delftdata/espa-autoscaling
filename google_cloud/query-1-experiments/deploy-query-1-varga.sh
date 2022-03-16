2ae264afa11f1e2fc2ed97321ac19e2c96710ed8
wybekoper <wybe.koper@gmail.com>
2022-03-16 19:21:13 +0100

    Updated varga deployment script

--- google_cloud/query-1-experiments/deploy-query-1-varga.sh
+++ google_cloud/query-1-experiments/deploy-query-1-varga.sh
@@ -10,7 +10,7 @@ helm repo update
 helm install my-release prometheus-community/prometheus-adapter
 kubectl delete deployment/my-release-prometheus-adapter
 
-kubectl apply -f prometheus-adapter-config.yaml
+kubectl apply -f prometheus-adapter-config_varga_v2.yaml
 kubectl apply -f adapter-deployment.yaml
 kubectl apply -f varga_HPA.yaml
 
