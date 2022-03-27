9b899d5a888392b8805512040ac596ee4fc51998
wybekoper <wybe.koper@gmail.com>
2022-03-27 22:26:05 +0200

    collected cosine query 3 data

--- data_processing/a_preprocessing.py
+++ data_processing/a_preprocessing.py
@@ -2,8 +2,8 @@ from b_get_experiment_results import scrape_data
 from c_preprocess_data import combine_all_metrics
 from d_plot_time_series import plot_experiments
 
-prometheus_ip = "34.65.120.182"
-query = "query-1"
+prometheus_ip = "35.203.60.252"
+query = "query-3"
 load_pattern = "cosine"
 autoscaler = "ds2-adapted-reactive"
 metric = "66"
