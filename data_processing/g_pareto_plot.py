f96d7dc54ae12c1aac2e85f7bc7de15652c9d041
wybekoper <wybe.koper@gmail.com>
2022-03-29 16:54:49 +0200

    Added tables generation script

--- data_processing/g_pareto_plot.py
+++ data_processing/g_pareto_plot.py
@@ -7,106 +7,62 @@ from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
 
-load_pattern = "cosine"
-query = "query-1"
-path = "../experiment_data_processed/full_data/" + load_pattern + "/" + query
+def pareto_plot(query, zoomed, latency_limit, zoomed_latency_limit):
+    load_pattern = "cosine"
+    path = "../experiment_data_processed/full_data/" + load_pattern + "/" + query
+    files = os.listdir(path)
+    fig, ax = plt.subplots()
+    color_per_autoscaler ={"HPA": "red", "vargav1": "purple","vargav2":"orange", "dhalion": "green", "ds2":"black", "ds2-adapted-reactive": "pink", "ds2-original-reactive":"brown", "ds2-adapted-non-reactive":"blue", "ds2-original-non-reactive":"blue"}
 
 
-files = os.listdir(path)
+    latency_per_autoscaler = []
+    taskmanagers_per_autoscaler = []
+    texts = []
+    seen = set()
+    for file in files:
+        file_info = file.split("_")
+        query = file_info[0]
+        auto_scaler = file_info[1]
+        if "non" in auto_scaler:
+            continue
+        metric = file_info[2].replace(".csv", "")
+        df = pd.read_csv("../experiment_data_processed/full_data/" + load_pattern + "/" + query + "/" + file)
+        latency_list = df['latency'].tolist()
+        taskmanager_list = df['taskmanager'].tolist()
+        average_latency = sum(latency_list) / len(latency_list)
+        average_taskmanager = sum(taskmanager_list) / len(taskmanager_list)
+        if average_latency > latency_limit:
+            continue
+        if zoomed and average_latency > zoomed_latency_limit:
+            continue
+        latency_per_autoscaler.append(average_latency)
+        taskmanagers_per_autoscaler.append(average_taskmanager)
+        ax.scatter(average_taskmanager,average_latency, s=50, color=color_per_autoscaler[auto_scaler], label=auto_scaler if auto_scaler not in seen else "")
+        # ax.annotate(metric, (average_taskmanager, average_latency), ha='center', size=6)
+        seen.add(auto_scaler)
+        texts.append(ax.text(average_taskmanager, average_latency, metric, ha='right', va='top', size=10))
 
-fig, ax = plt.subplots()
-# color_per_autoscaler ={"HPA": "red", "vargav1": "purple","vargav2":"orange", "dhalion": "green", "ds2":"black"}
-color_per_autoscaler ={"HPA": "red", "vargav1": "purple","vargav2":"orange", "dhalion": "green", "ds2":"black", "ds2-adapted-reactive": "pink", "ds2-original-reactive":"brown", "ds2-adapted-non-reactive":"blue", "ds2-original-non-reactive":"blue"}
+    if zoomed:
+        plt.ylim([0,zoomed_latency_limit])
+        plt.xlim([0,16])
+    else:
+        plt.ylim([0,latency_limit])
+        plt.xlim([0,16])
 
+    adjust_text(texts, only_move={'points':'y', 'texts':'y'}, arrowprops=dict(arrowstyle="->", color='r', lw=0))
+    plt.legend(loc=(1.02,0.5), labelspacing=1)
+    plt.grid()
+    plt.xlabel("Average number of taskmanagers")
+    plt.ylabel("Average latency (s)")
 
-latency_per_autoscaler = []
-taskmanagers_per_autoscaler = []
-texts = []
-seen = set()
-for file in files:
-    file_info = file.split("_")
-    query = file_info[0]
-    auto_scaler = file_info[1]
-    if "non" in auto_scaler:
-        continue
-    metric = file_info[2].replace(".csv", "")
-    df = pd.read_csv("../experiment_data_processed/full_data/" + load_pattern + "/" + query + "/" + file)
-    latency_list = df['latency'].tolist()
-    taskmanager_list = df['taskmanager'].tolist()
-    average_latency = sum(latency_list) / len(latency_list)
-    average_taskmanager = sum(taskmanager_list) / len(taskmanager_list)
-    latency_per_autoscaler.append(average_latency)
-    taskmanagers_per_autoscaler.append(average_taskmanager)
-    ax.scatter(average_taskmanager,average_latency, s=20, color=color_per_autoscaler[auto_scaler], label=auto_scaler if auto_scaler not in seen else "")
-    # ax.annotate(metric, (average_taskmanager, average_latency), ha='center', size=6)
-    seen.add(auto_scaler)
-    texts.append(ax.text(average_taskmanager, average_latency, metric, ha='center', va='center', size=6))
-
-plt.ylim([0,20])
-plt.xlim([0,16])
-# adjust_text(texts)
-# adjust_text(texts, arrowprops=dict(arrowstyle='->', color='red'))
-adjust_text(texts, only_move={'points':'y', 'texts':'y'}, arrowprops=dict(arrowstyle="->", color='r', lw=0))
-plt.legend(loc=(1.02,0.5), labelspacing=1)
-plt.grid()
-plt.xlabel("Average number of taskmanagers")
-plt.ylabel("Average latency (s)")
-
-# Make the zoom-in plot:
-# axins = zoomed_inset_axes(ax, 3, loc=1) # zoom = 2
-# axins.scatter(1,1)
-# axins.set_xlim(0, 5)
-# axins.set_ylim(0, 5)
-# plt.xticks(visible=False)
-# plt.yticks(visible=False)
-# plt.draw()
-# plt.show()
-
-path = "../figures_final/" + load_pattern + "/" + query + "/pareto_figs/" + query + "_pareto.png"
-plt.savefig(path, format="png", bbox_inches=Bbox([[0, 0], [8.0, 5.0]]), dpi=600)
+    if zoomed:
+        path = "../figures_final/" + load_pattern + "/" + query + "/pareto_figs/" + query + "_pareto_zoomed.png"
+    else:
+        path = "../figures_final/" + load_pattern + "/" + query + "/pareto_figs/" + query + "_pareto.png"
+    plt.savefig(path, format="png", bbox_inches=Bbox([[0, 0], [8.0, 5.0]]), dpi=600)
 
 
-#
-# query = "query-1"
-#
-# HPA_data = ["05", "70", "90", "05", "70", "90", "01", "02", "03"]
-# latency_per_autoscaler = []
-# taskmanagers_per_autoscaler = []
-#
-# color_per_autoscaler ={"HPA": "red", "varga": "purple", "dhalion": "green"}
-#
-# fig, ax = plt.subplots()
-#
-# auto_scaler = "HPA"
-# texts = []
-# for i in range(0, 9):
-#     if i == 3:
-#         auto_scaler = "varga"
-#     elif i == 6:
-#         auto_scaler = "dhalion"
-#     df = pd.read_csv("../experiment_data_processed/full_data/" + query + "_" + auto_scaler + "_" + HPA_data[i] + ".csv")
-#     latency_list = df['latency'].tolist()
-#     taskmanager_list = df['taskmanager'].tolist()
-#     average_latency = sum(latency_list) / len(latency_list)
-#     average_taskmanager = sum(taskmanager_list) / len(taskmanager_list)
-#     latency_per_autoscaler.append(average_latency)
-#     taskmanagers_per_autoscaler.append(average_taskmanager)
-#     ax.scatter(average_taskmanager,average_latency, s=20, color=color_per_autoscaler[auto_scaler], label=auto_scaler if i % 3 == 0 else "")
-#     # ax.annotate(auto_scaler + " " + HPA_data[i], (average_taskmanager, average_latency-1), ha='center')
-#     texts.append(ax.text(average_taskmanager, average_latency, auto_scaler + " " + HPA_data[i], ha='center', va='center'))
-#
-# adjust_text(texts, arrowprops=dict(arrowstyle='->', color='red'))
-# # adjust_text(texts, only_move={'points':'y', 'text':'y'}, arrowprops=dict(arrowstyle="->", color='r', lw=0.5))
-#
-# plt.legend(loc=(1.02,0.85), labelspacing=1)
-# plt.grid()
-# plt.xlabel("Average number of taskmanagers")
-# plt.ylabel("Average latency (s)")
-# # plt.show()
-#
-#
-# # color =["red", "cyan", "purple", "green"]
-#
-#
-# path = "../figures_final/cosine/query-1/pareto_figs/" + query + "_pareto.png"
-# plt.savefig(path, format="png", bbox_inches=Bbox([[0, 0], [6.0, 5.0]]), dpi=600)
+pareto_plot("query-3", False, 200, 20)
+
+pareto_plot("query-3", True, 200, 20)
+
---
+++ data_processing/i_tables.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+from matplotlib.transforms import Bbox
+import pandas as pd
+from adjustText import adjust_text
+import os
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+
+def number_of_rescales(taskmanager):
+    previous_number_taskmanagers = taskmanager[0]
+    scaling_events = 0
+    for val in taskmanager:
+        if val != previous_number_taskmanagers:
+            scaling_events += 1
+        previous_number_taskmanagers = val
+    return scaling_events
+
+query = "query-3"
+load_pattern = "cosine"
+path = "../experiment_data_processed/full_data/" + load_pattern + "/" + query
+files = os.listdir(path)
+
+
+pd.set_option('precision', 2)
+
+name = []
+metric_per_autoscaler = []
+latency_per_autoscaler = []
+taskmanagers_per_autoscaler = []
+combined_per_autoscaler = []
+rescales_per_autoscaler = []
+seen = set()
+for file in files:
+    file_info = file.split("_")
+    query = file_info[0]
+    auto_scaler = file_info[1]
+    if "non" in auto_scaler:
+        continue
+    metric = file_info[2].replace(".csv", "")
+    df = pd.read_csv("../experiment_data_processed/full_data/" + load_pattern + "/" + query + "/" + file)
+    latency_list = df['latency'].tolist()
+    taskmanager_list = df['taskmanager'].tolist()
+    average_latency = sum(latency_list) / len(latency_list)
+    average_taskmanager = sum(taskmanager_list) / len(taskmanager_list)
+    rescales = number_of_rescales(df['taskmanager'].tolist())
+    name.append(auto_scaler)
+    metric_per_autoscaler.append(metric)
+    taskmanagers_per_autoscaler.append(average_taskmanager)
+    latency_per_autoscaler.append(average_latency)
+    combined_per_autoscaler.append(average_latency + average_taskmanager)
+    rescales_per_autoscaler.append(rescales)
+
+    # print(auto_scaler + " " + metric + " " + "{:.2f} {:.2f} {:.2f} {:.0f}".format(average_latency, average_taskmanager, average_latency + average_taskmanager, rescales))
+
+data = pd.DataFrame({"Auto-scaler": name, "Metric value": metric_per_autoscaler, "Latency (s)": latency_per_autoscaler, "Taskmanagers": taskmanagers_per_autoscaler, "Latency + Taskmanagers": combined_per_autoscaler, "Scaling operations": rescales_per_autoscaler})
+data = data.sort_values(by="Latency + Taskmanagers")
+print(data.to_string(index=False))
