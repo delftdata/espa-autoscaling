d89fdb58673a6288970d32806a68a646f718ee9f
Job Kanis <jobkanis@gmail.com>
2022-09-16 10:21:49 +0200

    Update claim.yaml

--- _my_shell_scripts/gc_setup.sh
+++ _my_shell_scripts/gc_setup.sh
@@ -41,3 +41,4 @@ gcloud container clusters resize $CLUSTER --num-nodes 7
 gcloud auth login jobkanis@gmail.com
 gcloud cloud-shell ssh --authorize-session
 gcloud container clusters create pasaf-experiments --zone europe-west4-a --machine-type "e2-standard-8" --num-nodes=3 --disk-size=100
+gcloud container clusters delete pasaf-experiments --zone europe-west4-a
