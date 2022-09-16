9fd715ecea5a194e579ce79455a4a2bfbff8edfb
Job Kanis <jobkanis@gmail.com>
2022-09-16 11:46:24 +0200

    Update deploy_nfs.sh

--- google_cloud/.my_shell_scripts/deploy_nfs.sh
+++ google_cloud/.my_shell_scripts/deploy_nfs.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Deploy nfs-service
+echo "Creating nfs-server service"
 kubectl apply -f ./nfs/first-claim.yaml
 kubectl apply -f ./nfs/nfs-service.yaml
 
+
 # wait for service to load
-kubectl wait --timeout=2m --for=condition=ready statefulset --all
+echo "Waiting for nfs-server service to be ready"
+kubectl wait --timeout=3m --for=condition=ready pods --all
 
 # Get nfs IP and claim persistent memory
+echo "Setting up nfs server"
 export NFS_SERVICE_IP=$(kubectl get svc nfs-server -o yaml | grep clusterIP | awk '{print $2}')
-
 envsubst < ./nfs/claim.yaml | kubectl apply -f -
 
 # Deploy nfs-server
