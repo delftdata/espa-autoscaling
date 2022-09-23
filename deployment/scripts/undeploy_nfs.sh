2b78dd17502c5b672f6d023b8574566da9aecc24
Job Kanis <jobkanis@gmail.com>
2022-09-23 11:33:06 +0200

    Undeployment

--- deployment/scripts/undeploy_nfs.sh
+++ deployment/scripts/undeploy_nfs.sh
@@ -5,15 +5,15 @@
 echo "Undeploying nfs server"
 
 # Delete all pvc and pv for parallel execution
-kubectl delete pvc --all
-kubectl delete pv --all
-kubectl delete --wait=true -f nfs-service.yaml
-kubectl delete --wait=true -f nfs.yaml
-
-# Delete only nfs's pvc and pv non-parallel
-#export NFS_SERVICE_IP=$(kubectl get svc nfs-server -o yaml | grep clusterIP | awk '{print $2}')
-#kubectl delete --wait=true -f nfs-first-claim.yaml
+#kubectl delete pvc --all
+#kubectl delete pv --all
 #kubectl delete --wait=true -f nfs-service.yaml
-#envsubst < nfs-claim.yaml | kubectl delete --wait=true -f -
 #kubectl delete --wait=true -f nfs.yaml
+
+# Delete only nfs's pvc's and pv's
+export NFS_SERVICE_IP=$(kubectl get svc nfs-server -o yaml | grep clusterIP | awk '{print $2}')
+kubectl delete --wait=false -f nfs-first-claim.yaml
+kubectl delete --wait=true -f nfs-service.yaml
+envsubst < nfs-claim.yaml | kubectl delete --wait=false -f -
+kubectl delete --wait=true -f nfs.yaml
 echo "Finished undeploying deploying nfs server"
