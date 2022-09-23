2a1ab2b5cb7658405e31f1a7ab96cdfe1328775d
Job Kanis <jobkanis@gmail.com>
2022-09-23 12:23:39 +0200

    Finalise deploymetn

--- deployment/scripts/run_experiments.sh
+++ deployment/scripts/run_experiments.sh
@@ -2,7 +2,7 @@
 
 echo "Deploying"
 source ./scripts/deploy_nfs.sh
-source ./scripts/deploy_query_1.sh
+source ./scripts/deploy_query.sh
 source ./scripts/deploy_autoscaler.sh
 
 echo "Finished deploying"
@@ -20,7 +20,7 @@ done
 
 echo "Undeploying"
 source ./scripts/undeploy_autoscaler.sh
-source ./scripts/undeploy_query_1.sh
+source ./scripts/undeploy_query.sh
 source ./scripts/undeploy_nfs.sh
 
 echo "Finished undeploying"
