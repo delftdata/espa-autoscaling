5dfd29a40dfa62e36ce28e0ad777c87ec3f85039
Job Kanis <jobkanis@gmail.com>
2022-09-23 15:51:49 +0200

    Finish consecutive experiment deployment

--- deployment/scripts/run_experiments.sh
+++ deployment/scripts/run_experiments.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
 input="experiments/experiments.txt"
+echo "Starting experiments from $input"
+
 while IFS= read -r line
 do
-  printf 'Run experiment: %s\n' "$line"
   IFS=';' read -ra ss <<< "$line"
   query="${ss[0]}"
   autoscaler="${ss[1]}"
   metric="${ss[2]}"
-  echo "Query: $query\nAutoscaler: $autoscaler\Metric: $metric\n"
-done
 
-echo "Deploying"
-#source ./scripts/deploy_nfs.sh
-#source ./scripts/deploy_queries.sh
-#source ./scripts/deploy_autoscaler.sh dhalion 05
+  echo "Deploying experiment with: Query=$query autoscaler=$autoscaler metric=$metric"
+  source ./scripts/deploy_nfs.sh
+  source ./scripts/deploy_queries.sh query
+  source ./scripts/deploy_autoscaler.sh $dhalion $metric
 
-echo "Running experiments"
+  wait 180
 
-echo "Fetchin results from prometheus server"
+  echo "Undeploying..."
+  source ./scripts/undeploy_autoscaler.sh $autoscaler
+  source ./scripts/undeploy_queries.sh $query
+  source ./scripts/undeploy_nfs.sh
+done < "$input"
 
+echo "Finished experiments"
 
-echo "Undeploying"
-#source ./scripts/undeploy_autoscaler.sh
-#source ./scripts/undeploy_queries.sh
-#source ./scripts/undeploy_nfs.sh
-
-echo "Finished undeploying"
