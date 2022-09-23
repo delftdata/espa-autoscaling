184e679948139fc5271025f1d700bd70b202ce64
Job Kanis <jobkanis@gmail.com>
2022-09-23 11:24:58 +0200

    Update undeployment

--- deployment/scripts/undeploy_nfs.sh
+++ deployment/scripts/undeploy_nfs.sh
@@ -3,8 +3,9 @@
 # Script assumes execution from the google_cloud directory
 
 echo "Undeploying nfs server"
-kubectl delete pvc --all
-kubectl delete pv --all
+export NFS_SERVICE_IP=$(kubectl get svc nfs-server -o yaml | grep clusterIP | awk '{print $2}')
+kubectl delete --wait=true -f nfs-first-claim.yaml
 kubectl delete --wait=true -f nfs-service.yaml
+envsubst < nfs-claim.yaml | kubectl delete --wait=true -f -
 kubectl delete --wait=true -f nfs.yaml
 echo "Finished undeploying deploying nfs server"
