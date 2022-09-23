96e62bce47bc27d9e96f361230617c8c63b1b8bb
Job Kanis <jobkanis@gmail.com>
2022-09-23 11:15:25 +0200

    Streamline nfs_undeployment

--- deployment/scripts/undeploy_nfs.sh
+++ deployment/scripts/undeploy_nfs.sh
@@ -4,13 +4,8 @@
 
 echo "Undeploying nfs server"
 export NFS_SERVICE_IP=$(kubectl get svc nfs-server -o yaml | grep clusterIP | awk '{print $2}')
-echo "step 1"
-kubectl delete --wait=true -f nfs-first-claim.yaml
-echo "step 2"
+kubectl delete -f nfs-first-claim.yaml
 kubectl delete --wait=true -f nfs-service.yaml
-echo "step 3"
 envsubst < nfs-claim.yaml | kubectl delete --wait=true -f -
-echo "step 4"
 kubectl delete --wait=true -f nfs.yaml
-echo "step 5"
 echo "Finished undeploying deploying nfs server"
