ed002b9c0ce6ea439c3fbbbb29f1b477c4a5343c
jorgeSia <georgedrfsia@gmail.com>
2022-11-01 22:21:53 +0100

    Add Query2KafkaSource

---
+++ experiments/src/main/java/ch/ethz/systems/strymon/ds2/flink/nexmark/queries/Query2KafkaSource.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.ethz.systems.strymon.ds2.flink.nexmark.queries;
+
+import ch.ethz.systems.strymon.ds2.common.BidDeserializationSchema;
+import ch.ethz.systems.strymon.ds2.flink.nexmark.sinks.DummyLatencyCountingSink;
+import org.apache.beam.sdk.nexmark.model.Bid;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.GenericTypeInfo;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Query2KafkaSource {
+
+    private static final Logger logger  = LoggerFactory.getLogger(Query2.class);
+
+    public static void main(String[] args) throws Exception {
+
+        // Checking input parameters
+        final ParameterTool params = ParameterTool.fromArgs(args);
+
+        // set up the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.disableOperatorChaining();
+
+        // enable latency tracking
+        // env.getConfig().setLatencyTrackingInterval(5000);
+
+        final int srcRate = params.getInt("srcRate", 100000);
+
+        final int max_parallelism_source = params.getInt("source-max-parallelism", 20);
+
+        KafkaSource<Bid> source =
+                KafkaSource.<Bid>builder()
+                        .setBootstrapServers("kafka-service:9092")
+                        .setTopics("bids_topic")
+                        .setGroupId("consumer_group")
+                        .setProperty("fetch.min.bytes", "1000")
+                        .setStartingOffsets(OffsetsInitializer.committedOffsets(OffsetResetStrategy.EARLIEST))
+                        .setValueOnlyDeserializer(new BidDeserializationSchema())
+                        .build();
+        
+        DataStream<Bid> bids = env.fromSource(source, WatermarkStrategy.noWatermarks(), "BidsSource")
+                                    .slotSharingGroup("BidsSource")
+                                    .setParallelism(params.getInt("p-source", 1))
+                                    .setMaxParallelism(max_parallelism_source)
+                                    .uid("BidsSource");
+
+        // SELECT Rstream(auction, price)
+        // FROM Bid [NOW]
+        // WHERE auction = 1007 OR auction = 1020 OR auction = 2001 OR auction = 2019 OR auction = 2087;
+
+        DataStream<Tuple2<Long, Long>> converted = bids
+                .flatMap(new FlatMapFunction<Bid, Tuple2<Long, Long>>() {
+                    @Override
+                    public void flatMap(Bid bid, Collector<Tuple2<Long, Long>> out) throws Exception {
+                        if(bid.auction % 1007 == 0 || bid.auction % 1020 == 0 || bid.auction % 2001 == 0 || bid.auction % 2019 == 0 || bid.auction % 2087 == 0) {
+                            out.collect(new Tuple2<>(bid.auction, bid.price));
+                        }
+                    }
+                }).setParallelism(params.getInt("p-flatMap", 1)).slotSharingGroup("FlatMap");
+
+        GenericTypeInfo<Object> objectTypeInfo = new GenericTypeInfo<>(Object.class);
+        converted.transform("DummyLatencySink", objectTypeInfo, new DummyLatencyCountingSink<>(logger))
+                .setParallelism(params.getInt("p-flatMap", 1)).slotSharingGroup("Sink");
+
+        // execute program
+        env.execute("Nexmark Query2 with a Kafka Source");
+    }
+
+}
