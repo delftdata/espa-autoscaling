0eae0e48220806068fd59589a8172e15b1d5e0f1
jorgeSia <georgedrfsia@gmail.com>
2022-11-01 21:52:41 +0100

    Transform to stateful join.

--- experiments/src/main/java/ch/ethz/systems/strymon/ds2/flink/nexmark/queries/Query3KafkaSource.java
+++ experiments/src/main/java/ch/ethz/systems/strymon/ds2/flink/nexmark/queries/Query3KafkaSource.java
@@ -26,11 +26,18 @@ import org.apache.beam.sdk.nexmark.model.Auction;
 import org.apache.beam.sdk.nexmark.model.Person;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.typeutils.GenericTypeInfo;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -42,7 +49,6 @@ import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
 import java.util.HashSet;
 
 public class Query3KafkaSource {
@@ -139,21 +145,42 @@ public class Query3KafkaSource {
     private static final class JoinPersonsWithAuctions extends RichCoFlatMapFunction<Auction, Person, Tuple4<String, String, String, Long>> {
 
         // person state: id, <name, city, state>
-        private HashMap<Long, Tuple3<String, String, String>> personMap = new HashMap<>();
+        private MapState<Long, Tuple3<String, String, String>> personMap;;
 
         // auction state: seller, List<id>
-        private HashMap<Long, HashSet<Long>> auctionMap = new HashMap<>();
+        private MapState<Long, HashSet<Long>> auctionMap;
+
+        @Override
+        public void open(Configuration parameters){
+            MapStateDescriptor<Long, Tuple3<String, String, String>> personDescriptor =
+            new MapStateDescriptor<Long, Tuple3<String, String, String>>(
+                    "person-map",
+                    BasicTypeInfo.LONG_TYPE_INFO,
+                    new TupleTypeInfo<>(BasicTypeInfo.STRING_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO, BasicTypeInfo.STRING_TYPE_INFO)
+                   );
+
+            personMap = getRuntimeContext().getMapState(personDescriptor);
+
+            MapStateDescriptor<Long, HashSet<Long>> auctionDescriptor =
+            new MapStateDescriptor<Long, HashSet<Long>>(
+                    "auction-map",
+                    BasicTypeInfo.LONG_TYPE_INFO,
+                    TypeInformation.of(new TypeHint<HashSet<Long>>(){})
+                   );
+
+            auctionMap = getRuntimeContext().getMapState(auctionDescriptor);
+        }
 
         @Override
         public void flatMap1(Auction auction, Collector<Tuple4<String, String, String, Long>> out) throws Exception {
             // check if auction has a match in the person state
-            if (personMap.containsKey(auction.seller)) {
+            if (personMap.contains(auction.seller)) {
                 // emit and don't store
                 Tuple3<String, String, String> match = personMap.get(auction.seller);
                 out.collect(new Tuple4<>(match.f0, match.f1, match.f2, auction.id));
             } else {
                 // we need to store this auction for future matches
-                if (auctionMap.containsKey(auction.seller)) {
+                if (auctionMap.contains(auction.seller)) {
                     HashSet<Long> ids = auctionMap.get(auction.seller);
                     ids.add(auction.id);
                     auctionMap.put(auction.seller, ids);
@@ -171,9 +198,10 @@ public class Query3KafkaSource {
             personMap.put(person.id, new Tuple3<>(person.name, person.city, person.state));
 
             // check if person has a match in the auction state
-            if (auctionMap.containsKey(person.id)) {
+            if (auctionMap.contains(person.id)) {
                 // output all matches and remove
-                HashSet<Long> auctionIds = auctionMap.remove(person.id);
+                HashSet<Long> auctionIds = auctionMap.get(person.id);
+                auctionMap.remove(person.id);
                 for (Long auctionId : auctionIds) {
                     out.collect(new Tuple4<>(person.name, person.city, person.state, auctionId));
                 }
