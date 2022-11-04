8f9192dfe0ec3abb56397337eb5ab192d4329b55
jorgeSia <georgedrfsia@gmail.com>
2022-11-04 13:57:34 +0100

    Add mode in deploy_queries script + placeholders

--- deployment/scripts/deploy_queries.sh
+++ deployment/scripts/deploy_queries.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-QUERY=$1 #{1, 3, 11}
+QUERY=$1 #{1, 2, 3, 5, 8, 11}
+MODE=$2 #{reactive, non-reactive}
 echo "Deploying query $QUERY"
 
 # common
-kubectl apply -f flink-configuration-configmap.yaml
+if [$MODE = "reactive"]
+then 
+  kubectl apply -f flink-configuration-configmap.yaml
+else
+  kubectl apply -f flink-configuration-configmap-non-reactive.yaml
+fi
 kubectl apply -f jobmanager-rest-service.yaml
 kubectl apply -f jobmanager-service.yaml
 kubectl apply -f experiments-taskmanager.yaml
@@ -24,21 +30,71 @@ kubectl wait --timeout=3m --for=condition=ready pods --all
 case $QUERY in
   1)
     kubectl exec kafka-2 -- /opt/kafka/bin/kafka-topics.sh --create -zookeeper zoo1:2181  --replication-factor 1 --partitions 24 --topic bids_topic
-    kubectl apply -f query1-experiments-jobmanager.yaml
+    if [$MODE = "reactive"]
+    then
+      kubectl apply -f query1-experiments-jobmanager.yaml
+    else
+      kubectl apply -f query1-experiments-jobmanager-non-reactive.yaml
+    fi
     kubectl apply -f query1-workbench-deployment.yaml
+
+  ;;
+  2) # TODO: all the scripts for query 2
+    kubectl exec kafka-2 -- /opt/kafka/bin/kafka-topics.sh --create -zookeeper zoo1:2181  --replication-factor 1 --partitions 24 --topic bids_topic
+    if [$MODE = "reactive"]
+    then
+      kubectl apply -f query2-experiments-jobmanager.yaml
+    else
+      kubectl apply -f query2-experiments-jobmanager-non-reactive.yaml
+    fi
+    kubectl apply -f query2-workbench-deployment.yaml 
+
   ;;
   3)
     kubectl exec kafka-2 -- /opt/kafka/bin/kafka-topics.sh --create -zookeeper zoo1:2181  --replication-factor 1 --partitions 24 --topic auction_topic
     kubectl exec kafka-2 -- /opt/kafka/bin/kafka-topics.sh --create -zookeeper zoo1:2181  --replication-factor 1 --partitions 24 --topic person_topic
+    if [$MODE = "reactive"]
+    then
+      kubectl apply -f query3-experiments-jobmanager.yaml
+    else
+      kubectl apply -f query3-experiments-jobmanager-non-reactive.yaml
 
-    kubectl apply -f query3-experiments-jobmanager.yaml
+    fi
     kubectl apply -f query3-workbench-deployment.yaml
-  ;;
 
+  ;;
+  5) # TODO: all the scripts for query 5
+    kubectl exec kafka-2 -- /opt/kafka/bin/kafka-topics.sh --create -zookeeper zoo1:2181  --replication-factor 1 --partitions 24 --topic bids_topic
+    if [$MODE = "reactive"]
+    then
+      kubectl apply -f query5-experiments-jobmanager.yaml
+    else
+      kubectl apply -f query5-experiments-jobmanager-non-reactive.yaml
+    fi
+    kubectl apply -f query5-workbench-deployment.yaml
+
+  ;;
+  8) # TODO: all the scripts for query 8
+    kubectl exec kafka-2 -- /opt/kafka/bin/kafka-topics.sh --create -zookeeper zoo1:2181  --replication-factor 1 --partitions 24 --topic bids_topic
+    if [$MODE = "reactive"]
+    then
+      kubectl apply -f query8-experiments-jobmanager.yaml
+    else
+      kubectl apply -f query8-experiments-jobmanager-non-reactive.yaml
+    fi
+    kubectl apply -f query8-workbench-deployment.yaml
+
+  ;;
   11)
     kubectl exec kafka-2 -- /opt/kafka/bin/kafka-topics.sh --create -zookeeper zoo1:2181  --replication-factor 1 --partitions 24 --topic bids_topic
-    kubectl apply -f query11-experiments-jobmanager.yaml
+    if [$MODE = "reactive"]
+    then
+      kubectl apply -f query11-experiments-jobmanager.yaml
+    else
+      kubectl apply -f query11-experiments-jobmanager-non-reactive.yaml
+    fi
     kubectl apply -f query11-workbench-deployment.yaml
+
   ;;
   *)
 esac
