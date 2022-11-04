fcc26fd1de3b20baa30ff6d7cc43da1f9b1374c1
jorgeSia <georgedrfsia@gmail.com>
2022-11-04 14:56:01 +0100

    Add mode as a parameter in deploy_experiment.sh

--- deployment/scripts/deploy_experiment.sh
+++ deployment/scripts/deploy_experiment.sh
@@ -3,9 +3,10 @@
 query=$1
 autoscaler=$2
 metric=$3
+mode=$4
 
 echo "Deploying experiment with: Query=$query autoscaler=$autoscaler metric=$metric"
 source ./scripts/deploy_nfs.sh
-source ./scripts/deploy_queries.sh $query
-source ./scripts/deploy_autoscaler.sh $autoscaler $metric $query
+source ./scripts/deploy_queries.sh $query $mdoe
+source ./scripts/deploy_autoscaler.sh $autoscaler $metric $query $mode
 echo "Finished deployment"
