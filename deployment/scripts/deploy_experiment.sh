732a67ddf088d5b3d3765a9fc6aedf7742ea8b65
jorgeSia <georgedrfsia@gmail.com>
2022-11-07 14:13:32 +0100

    Fix paths.

--- deployment/scripts/deploy_experiment.sh
+++ deployment/scripts/deploy_experiment.sh
@@ -6,7 +6,7 @@ metric=$3
 mode=$4
 
 echo "Deploying experiment with: Query=$query autoscaler=$autoscaler metric=$metric"
-source ./scripts/deploy_nfs.sh
-source ./scripts/deploy_queries.sh $query $mdoe
-source ./scripts/deploy_autoscaler.sh $autoscaler $metric $query $mode
+source ./deploy_nfs.sh
+source ./deploy_queries.sh $query $mode
+source ./deploy_autoscaler.sh $autoscaler $metric $query $mode
 echo "Finished deployment"
--- deployment/scripts/undeploy_experiment.sh
+++ deployment/scripts/undeploy_experiment.sh
@@ -5,8 +5,8 @@ autoscaler=$2
 mode=$3
 
 echo "Undeploying experiment Query=$query autoscaler=$autoscaler"
-source ./scripts/undeploy_autoscaler.sh $autoscaler $mode
-source ./scripts/undeploy_queries.sh $query $mode
+source ./undeploy_autoscaler.sh $autoscaler $mode
+source ./undeploy_queries.sh $query $mode
 sleep 60s
-source ./scripts/undeploy_nfs.sh
+source ./undeploy_nfs.sh
 echo "Finished undeploying experiment"
